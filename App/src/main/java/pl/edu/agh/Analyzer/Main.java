dc09dad883aad67e5158a12a9c6b62f5946c4b82
Paweł Nowak <pnowak@student.agh.edu.pl>
2017-07-07 22:20:16 +0200

    Added project skeleton

---
+++ App/src/main/java/pl/edu/agh/Analyzer/Main.java
@@ -0,0 +1,16 @@
+package pl.edu.agh.Analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by pawel on 07.07.17.
+ */
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
---
+++ App/src/main/java/pl/edu/agh/Analyzer/controller/TryController.java
@@ -0,0 +1,17 @@
+package pl.edu.agh.Analyzer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by pawel on 07.07.17.
+ */
+
+@Controller
+public class TryController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+}
