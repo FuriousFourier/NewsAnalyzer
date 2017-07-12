8a038e61541178bd719bc418ac7650e8d41c5f08
rockzzz <k.mura121@gmail.com>
2017-07-12 09:46:38 +0200

    Add fetching from DB (using CLI)

--- App/src/main/java/Analyzer/repository/PressReleaseRepository.java
+++ App/src/main/java/Analyzer/repository/PressReleaseRepository.java
@@ -1,8 +1,10 @@
 package Analyzer.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import Analyzer.model.Feed;
 import Analyzer.model.PressRelease;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Date;
 import java.util.Set;
@@ -11,12 +13,9 @@ import java.util.Set;
  * Created by pawel on 11.07.17.
  */
 public interface PressReleaseRepository extends PagingAndSortingRepository<PressRelease, Integer> {
-
-    Set<PressRelease> findByFeed(Feed feed);
-
+  Set<PressRelease> findByFeed(Feed feed);
 	Set<PressRelease> findByTitle(String name);
 
-/*
-	PressRelease findByTitleAndDateAndFeed(String title, Date date, Feed feed);
-*/
+  @Query("SELECT r FROM PressRelease r WHERE MONTH(r.date)=:month AND YEAR(r.date)=:year")
+  List<PressRelease> findByMonthAndYear(@Param("month") String month, @Param ("year") String year);
 }
